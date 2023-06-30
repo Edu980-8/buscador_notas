c5daf419199aadd97913153ccb67a98d42075a95
Edu980-8 <daniedu151@gmail.com>
2023-06-29 21:18:28 -0500

    Cargue de agregacion de notas

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [nota, setNota] = useState("");
+  const [notas, setNotas] = useState([]);
+
+  const agregarNota = () => {
+    if (nota.trim() !== "") {
+      setNotas([...notas, nota]);
+      setNota("");
+    }
+  };
+
+  const eliminarNota = (index) => {
+    const nuevasNotas = [...notas];
+    nuevasNotas.splice(index, 1);
+    setNotas(nuevasNotas);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        className="text_input"
+        placeholder="Agregue su nota"
+        value={nota}
+        onChange={(e) => setNota(e.target.value)}
+      />
+      <h3 className="descripcion">
+        Recuerde que para agregar una nota debe seleccionar el botón agregar
+      </h3>
+      <p className="parrafo_nota">La nota que deseas agregar es: {nota} ?</p>
+      <div className="container_buttons">
+        <button className="success" type="submit" onClick={agregarNota}>
+          +
+        </button>
+      </div>
+      {notas.length > 0 && (
+        <table className="tabla_notas">
+          <thead>
+            <tr>
+              <th>Índice</th>
+              <th>Nota</th>
+              <th>Acciones</th>
+            </tr>
+          </thead>
+          <tbody>
+            {notas.map((nota, index) => (
+              <tr key={index}>
+                <td>{index}</td>
+                <td>{nota}</td>
+                <td>
+                  <button
+                    className="danger"
+                    onClick={() => eliminarNota(index)}
+                  >
+                    -
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 }
